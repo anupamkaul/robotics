27ba7da9cfe118b8c02b4eb1c27c0498da0b90ea
Anupam Kaul <anupam.kaul@yahoo.com>
2021-09-04 11:13:38 -0700

    Add helper comments to RotInv function

--- modern_robotics/ModernRobotics/packages/Python/tests/test1.py
+++ modern_robotics/ModernRobotics/packages/Python/tests/test1.py
@@ -1,17 +1,25 @@
 ## import the package
 import modern_robotics as mr
 
-## use some functions - use the helper function for RotInv from package:
+## use some functions - use the helper function for RotInv from package: (q to quit help)
 help(mr.RotInv)
 
-##next try this code: (q to quit)
+##next try this code: 
 import numpy as np
+
+# R : Rotation Matrix (defined with numpy)
+# invR : Inverse of R
 
 R = np.array([[0, 0, 1],
               [1, 0, 0],
               [0, 1, 0]])
 invR = mr.RotInv(R)
 print ("Var invR = \n",  invR)
+
+
+
+
+
 
 
 
