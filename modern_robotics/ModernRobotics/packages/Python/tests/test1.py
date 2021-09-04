c80a6631cecf7094d6c9abfd84d3920ac161698f
Anupam Kaul <anupam.kaul@yahoo.com>
2021-09-04 11:02:39 -0700

    Add test1 that invokes modern_robotics package and runs basic code
    This works and I can see the helper function plus np array manipulations

---
+++ modern_robotics/ModernRobotics/packages/Python/tests/test1.py
@@ -0,0 +1,17 @@
+## import the package
+import modern_robotics as mr
+
+## use some functions - use the helper function for RotInv from package:
+help(mr.RotInv)
+
+##next try this code: (q to quit)
+import numpy as np
+
+R = np.array([[0, 0, 1],
+              [1, 0, 0],
+              [0, 1, 0]])
+invR = mr.RotInv(R)
+
+
+
+
