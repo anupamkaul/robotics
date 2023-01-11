f9ad10812ca85725cc1bbc98e3eb5e3319f6526d
Anupam Kaul <anupam.kaul@yahoo.com>
2023-01-11 10:42:52 -0800

    Add run script to launch mujoco models
    List out different models in a file

---
+++ simulator/pre-builts/Run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# you can simulate xml files under the model dir using simulate, like so:
+
+cd mujoco-2.3.1/bin
+./simulate ../model/humanoid/humanoid.xml &
+
+# other pre-built models are:
+
+# ./balloons/balloons.xml
+# ./flag/flag.xml
+# ./mug/mug.xml
+# ./plugin/coil.xml
+# ./plugin/press.xml
+# ./plugin/cable.xml
+# ./plugin/floppy.xml
+# ./plugin/scene.xml
+# ./plugin/belt.xml
+# ./plugin/jelly.xml
+# ./humanoid100/humanoid_body.xml
+# ./humanoid100/humanoid100.xml
+# ./composite/particle_free.xml
+# ./composite/scene.xml
+# ./composite/rope.xml
+# ./composite/loop.xml
+# ./composite/grid2pin.xml
+# ./composite/particle_free2d.xml
+# ./composite/cloth.xml
+# ./composite/softbox.xml
+# ./composite/particle.xml
+# ./adhesion/active_adhesion.xml
+# ./slider_crank/slider_crank.xml
+# ./humanoid/humanoid.xml
+# ./humanoid/22_humanoids.xml
+# ./hammock/humanoid_body.xml
+# ./hammock/hammock.xml
+# ./tendon_arm/arm26.xml
+
+
+
