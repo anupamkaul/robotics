d779ef27210df39eafeeb0ca99e35825c48222b8
Anupam Kaul <anupam.kaul@yahoo.com>
2023-01-14 09:06:59 -0800

    Reading the MJPC paper and playing with MJPC models

--- simulator/pre-builts/Run.sh
+++ simulator/pre-builts/Run.sh
@@ -5,6 +5,9 @@
 cd mujoco-2.3.1/bin
 ./simulate ../model/humanoid/humanoid.xml &
 
+# note humanoid has 27DOF. 
+# MJPC: humanoid has 21 tasks and the task(goal) is 'standing' (RL/planners: iLQG, GD, Predictive Sampling)
+
 # other pre-built models are:
 
 # ./balloons/balloons.xml
@@ -73,5 +76,69 @@ cd mujoco-2.3.1/bin
 
 # ./simulate ../../../mujoco_menagerie/robotiq_2f85/scene.xml 
 # this is a good one for upwards object grasping (set the control menu on right to grasp and ungrasp with fingers)
+
+# ./simulate ../../../mujoco_menagerie/shadow_hand/scene_left.xml 
+# this is used to grasp an object with fingers of a hand
+
+# additional models from dm_control (with rl environments in mujoco):
+# (using $PATH_DM_CONTROL with ./simulate)
+
+# ./suite/quadruped.xml
+# ./suite/fish.xml
+# ./suite/common/materials.xml
+# ./suite/common/skybox.xml
+# ./suite/common/visual.xml
+# ./suite/walker.xml
+# ./suite/ball_in_cup.xml
+# ./suite/finger.xml
+# ./suite/pendulum.xml
+# ./suite/hopper.xml
+# ./suite/dog.xml
+# ./suite/manipulator.xml
+# ./suite/acrobot.xml
+# ./suite/cheetah.xml
+# ./suite/humanoid.xml
+# ./suite/reacher.xml
+# ./suite/stacker.xml
+# ./suite/point_mass.xml
+# ./suite/cartpole.xml
+# ./suite/lqr.xml
+# ./suite/humanoid_CMU.xml
+# ./suite/swimmer.xml
+# ./mujoco/testing/assets/model_with_ball_joints.xml
+# ./mujoco/testing/assets/arm.xml
+# ./mujoco/testing/assets/model_with_third_order_actuators.xml
+# ./mujoco/testing/assets/cartpole_no_names.xml
+# ./mujoco/testing/assets/humanoid.xml
+# ./mujoco/testing/assets/model_with_assets.xml
+# ./mujoco/testing/assets/cartpole.xml
+# ./mujoco/testing/assets/sphere.xml
+# ./entities/props/duplo/duplo2x4.xml
+# ./composer/arena.xml
+# ./mjcf/schema.xml
+# ./mjcf/test_assets/lego_brick.xml
+# ./mjcf/test_assets/included_with_invalid_filenames.xml
+# ./mjcf/test_assets/arena.xml
+# ./mjcf/test_assets/attribute_test_schema.xml
+# ./mjcf/test_assets/model_with_include.xml
+# ./mjcf/test_assets/included.xml
+# ./mjcf/test_assets/test_model.xml
+# ./mjcf/test_assets/model_with_assets.xml
+# ./mjcf/test_assets/model_with_invalid_filenames.xml
+# ./mjcf/test_assets/robot_arm.xml
+# ./mjcf/test_assets/model_with_nameless_assets.xml
+# ./third_party/kinova/jaco_arm.xml
+# ./third_party/kinova/jaco_hand.xml
+# ./third_party/kinova/common.xml
+# ./third_party/ant/ant.xml
+# ./locomotion/soccer/assets/boxhead/boxhead.xml
+# ./locomotion/walkers/assets/humanoid_CMU_V2019.xml
+# ./locomotion/walkers/assets/dog_v2/scene.xml
+# ./locomotion/walkers/assets/dog_v2/dog.xml
+# ./locomotion/walkers/assets/dog_v2/dog_base.xml
+# ./locomotion/walkers/assets/humanoid_CMU_V2020.xml
+# ./locomotion/walkers/assets/jumping_ball/jumping_ball_with_head.xml
+# ./locomotion/walkers/assets/rodent.xml
+
 
 
