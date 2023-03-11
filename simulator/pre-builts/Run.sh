7533ed0823f46d8ea5fb7da9c5acd94b64d90748
Anupam Kaul <anupam.kaul@yahoo.com>
2023-03-11 09:16:53 -0800

    Add dynamics books and papers

--- simulator/pre-builts/Run.sh
+++ simulator/pre-builts/Run.sh
@@ -83,62 +83,64 @@ cd mujoco-2.3.1/bin
 # additional models from dm_control (with rl environments in mujoco):
 # (using $PATH_DM_CONTROL with ./simulate)
 
-# ./suite/quadruped.xml
-# ./suite/fish.xml
-# ./suite/common/materials.xml
-# ./suite/common/skybox.xml
-# ./suite/common/visual.xml
-# ./suite/walker.xml
-# ./suite/ball_in_cup.xml
-# ./suite/finger.xml
-# ./suite/pendulum.xml
-# ./suite/hopper.xml
-# ./suite/dog.xml
-# ./suite/manipulator.xml
-# ./suite/acrobot.xml
-# ./suite/cheetah.xml
-# ./suite/humanoid.xml
-# ./suite/reacher.xml
-# ./suite/stacker.xml
-# ./suite/point_mass.xml
-# ./suite/cartpole.xml
-# ./suite/lqr.xml
-# ./suite/humanoid_CMU.xml
-# ./suite/swimmer.xml
-# ./mujoco/testing/assets/model_with_ball_joints.xml
-# ./mujoco/testing/assets/arm.xml
-# ./mujoco/testing/assets/model_with_third_order_actuators.xml
-# ./mujoco/testing/assets/cartpole_no_names.xml
-# ./mujoco/testing/assets/humanoid.xml
-# ./mujoco/testing/assets/model_with_assets.xml
-# ./mujoco/testing/assets/cartpole.xml
-# ./mujoco/testing/assets/sphere.xml
-# ./entities/props/duplo/duplo2x4.xml
-# ./composer/arena.xml
-# ./mjcf/schema.xml
-# ./mjcf/test_assets/lego_brick.xml
-# ./mjcf/test_assets/included_with_invalid_filenames.xml
-# ./mjcf/test_assets/arena.xml
-# ./mjcf/test_assets/attribute_test_schema.xml
-# ./mjcf/test_assets/model_with_include.xml
-# ./mjcf/test_assets/included.xml
-# ./mjcf/test_assets/test_model.xml
-# ./mjcf/test_assets/model_with_assets.xml
-# ./mjcf/test_assets/model_with_invalid_filenames.xml
-# ./mjcf/test_assets/robot_arm.xml
-# ./mjcf/test_assets/model_with_nameless_assets.xml
-# ./third_party/kinova/jaco_arm.xml
-# ./third_party/kinova/jaco_hand.xml
-# ./third_party/kinova/common.xml
-# ./third_party/ant/ant.xml
-# ./locomotion/soccer/assets/boxhead/boxhead.xml
-# ./locomotion/walkers/assets/humanoid_CMU_V2019.xml
-# ./locomotion/walkers/assets/dog_v2/scene.xml
-# ./locomotion/walkers/assets/dog_v2/dog.xml
-# ./locomotion/walkers/assets/dog_v2/dog_base.xml
-# ./locomotion/walkers/assets/humanoid_CMU_V2020.xml
-# ./locomotion/walkers/assets/jumping_ball/jumping_ball_with_head.xml
-# ./locomotion/walkers/assets/rodent.xml
+# ./simulate ../../../dm_control/dm_control/suite/quadruped.xml
+
+#./dm_control/dm_control/suite/quadruped.xml
+#./dm_control/dm_control/suite/fish.xml
+#./dm_control/dm_control/suite/common/materials.xml
+#./dm_control/dm_control/suite/common/skybox.xml
+#./dm_control/dm_control/suite/common/visual.xml
+#./dm_control/dm_control/suite/walker.xml
+#./dm_control/dm_control/suite/ball_in_cup.xml
+#./dm_control/dm_control/suite/finger.xml
+#./dm_control/dm_control/suite/pendulum.xml
+#./dm_control/dm_control/suite/hopper.xml
+#./dm_control/dm_control/suite/dog.xml
+#./dm_control/dm_control/suite/manipulator.xml
+#./dm_control/dm_control/suite/acrobot.xml
+#./dm_control/dm_control/suite/cheetah.xml
+#./dm_control/dm_control/suite/humanoid.xml
+#./dm_control/dm_control/suite/reacher.xml
+#./dm_control/dm_control/suite/stacker.xml
+#./dm_control/dm_control/suite/point_mass.xml
+#./dm_control/dm_control/suite/cartpole.xml
+#./dm_control/dm_control/suite/lqr.xml
+#./dm_control/dm_control/suite/humanoid_CMU.xml
+#./dm_control/dm_control/suite/swimmer.xml
+#./dm_control/dm_control/mujoco/testing/assets/model_with_ball_joints.xml
+#./dm_control/dm_control/mujoco/testing/assets/arm.xml
+#./dm_control/dm_control/mujoco/testing/assets/model_with_third_order_actuators.xml
+#./dm_control/dm_control/mujoco/testing/assets/cartpole_no_names.xml
+#./dm_control/dm_control/mujoco/testing/assets/humanoid.xml
+#./dm_control/dm_control/mujoco/testing/assets/model_with_assets.xml
+#./dm_control/dm_control/mujoco/testing/assets/cartpole.xml
+#./dm_control/dm_control/mujoco/testing/assets/sphere.xml
+#./dm_control/dm_control/entities/props/duplo/duplo2x4.xml
+#./dm_control/dm_control/composer/arena.xml
+#./dm_control/dm_control/mjcf/schema.xml
+#./dm_control/dm_control/mjcf/test_assets/lego_brick.xml
+#./dm_control/dm_control/mjcf/test_assets/included_with_invalid_filenames.xml
+#./dm_control/dm_control/mjcf/test_assets/arena.xml
+#./dm_control/dm_control/mjcf/test_assets/attribute_test_schema.xml
+#./dm_control/dm_control/mjcf/test_assets/model_with_include.xml
+#./dm_control/dm_control/mjcf/test_assets/included.xml
+#./dm_control/dm_control/mjcf/test_assets/test_model.xml
+#./dm_control/dm_control/mjcf/test_assets/model_with_assets.xml
+#./dm_control/dm_control/mjcf/test_assets/model_with_invalid_filenames.xml
+#./dm_control/dm_control/mjcf/test_assets/robot_arm.xml
+#./dm_control/dm_control/mjcf/test_assets/model_with_nameless_assets.xml
+#./dm_control/dm_control/third_party/kinova/jaco_arm.xml
+#./dm_control/dm_control/third_party/kinova/jaco_hand.xml
+#./dm_control/dm_control/third_party/kinova/common.xml
+#./dm_control/dm_control/third_party/ant/ant.xml
+#./dm_control/dm_control/locomotion/soccer/assets/boxhead/boxhead.xml
+#./dm_control/dm_control/locomotion/walkers/assets/humanoid_CMU_V2019.xml
+#./dm_control/dm_control/locomotion/walkers/assets/dog_v2/scene.xml
+#./dm_control/dm_control/locomotion/walkers/assets/dog_v2/dog.xml
+#./dm_control/dm_control/locomotion/walkers/assets/dog_v2/dog_base.xml
+#./dm_control/dm_control/locomotion/walkers/assets/humanoid_CMU_V2020.xml
+#./dm_control/dm_control/locomotion/walkers/assets/jumping_ball/jumping_ball_with_head.xml
+#./dm_control/dm_control/locomotion/walkers/assets/rodent.xml
 
 
 
