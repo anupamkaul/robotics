54efdcd77ab4b1bde70b907374eb2c20729437f5
anupamkaul <anupam.kaul@yahoo.com>
2026-09-14 12:53:18 -0700

    Apply the trick of moving link1 above base to the simple 2 link human arm
    Remove the factor of 10 and 9 multipliers that were causing the bizzare lying down effects
    Bring back the cos equation to the upper limb
    
    Questions: Since I am still using dctrl[0] and dctrl[1] what happens to the base?
    
    This restores main_human_arm of 2 links to work as intended.

--- custom/simple-arm/main_human_arm.cpp
+++ custom/simple-arm/main_human_arm.cpp
@@ -54,16 +54,10 @@ int main() {
             // =================================================================
             // Angles are in Radians. 
             // Let's make the shoulder sway wide, and the elbow flex tightly.
-
-            // Values that make shoulder stationary and elbow move like a pendulum
-
-            //double target_shoulder = 0.6 * std::sin(d->time * 2.0);
-            //double target_elbow    = 0.9 * std::cos(d->time * 4.5);
-
  
             // Adding forces so it "breaks" the shoulder joint and causes it to move as well        
-            double target_shoulder =  9 * 0.6 * std::sin(d->time * 2.0);
-            double target_elbow    = 10 * 0.6 * std::sin(d->time * 2.0);
+            double target_shoulder =   0.6 * std::sin(d->time * 2.0);
+            double target_elbow    = 0.9 * std::cos(d->time * 4.5);
 
             // Direct input matrix coordinate register assignments
             d->ctrl[0] = target_shoulder;
